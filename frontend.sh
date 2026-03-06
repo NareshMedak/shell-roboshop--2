ea296f79fbfe5d0d90b365cc30585e0370aaaf99
naresh <medaknaresh66@gmail.com>
2026-03-06 17:08:09 +0530

    hello world

--- frontend.sh
+++ frontend.sh
@@ -1,67 +1,39 @@
 #!/bin/bash
 
-USERID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-LOGS_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-SCRIPT_DIR=$PWD
+source ./common.sh
+check_root
 
-mkdir -p $LOGS_FOLDER
-echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+dnf module disable nginx -y &>>$LOG_FILE
+VALIDATE $? "Disabling Default Nginx"
 
-#check the priveleges or not 
+dnf module enable nginx:1.24 -y &>>$LOG_FILE
+VALIDATE $? "Enabling Nginx:1.24"
 
-if [ $USERID -ne 0 ]
-then
-    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
-    exit 1 #give other than 0 upto 127
-else
-    echo "You are running with root access" | tee -a $LOG_FILE
-fi
+dnf install nginx -y &>>$LOG_FILE
+VALIDATE $? "Installing Nginx"
 
-# validate functions takes input as exit status, what command they tried to install
-VALIDATE(){
-    if [ $1 -eq 0 ]
-    then
-        echo -e  "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
-    else
-        echo -e  "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
-        exit 1
-    fi
-}
+systemctl enable nginx  &>>$LOG_FILE
+systemctl start nginx 
+VALIDATE $? "Starting Nginx"
 
-dnf module disable nginx -y
-VALIDATE $? "Disabling the nginx"
+rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
+VALIDATE $? "Removing default content"
 
-dnf module enable nginx:1.24 -y
-VALIDATE $? "Enabling the nginx"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOG_FILE
+VALIDATE $? "Downloading frontend"
 
-dnf install nginx -y
-VALIDATE $? "Installing nginx"
+cd /usr/share/nginx/html 
+unzip /tmp/frontend.zip &>>$LOG_FILE
+VALIDATE $? "unzipping frontend"
 
-systemctl enable nginx 
-systemctl start nginx
-VALIDATE $? "Starting nginx"
+rm -rf /etc/nginx/nginx.conf &>>$LOG_FILE
+VALIDATE $? "Remove default nginx conf"
 
-rm -rf /usr/share/nginx/html/* 
-VALIDATE $? "Removing the default content"
+cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
+VALIDATE $? "Copying nginx.conf"
 
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
-VALIDATE $? "Downloadig frontend"
+systemctl restart nginx 
+VALIDATE $? "Restarting nginx"
 
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
-VALIDATE $? "Unzipping the frontend"
+print_time
 
-rm -rf /etc/nginx/nginx.conf
-VALIDATE $? "Removing the default content"
-
-cp  $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
-VALIDATE $? "Copying nignix.conf"
-
-systemctl restart nginx
-VALIDATE $? "Restart nginx"
--- payment.sh
+++ payment.sh
@@ -1,78 +1,31 @@
 #!/bin/bash
 
-START_TIME=$(date +%s)
-USERID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-LOGS_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-SCRIPT_DIR=$PWD
+source ./common.sh
+app_name=shipping
 
-mkdir -p $LOGS_FOLDER
-echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+check_root
+echo "Please enter root password to setup"
+read -s MYSQL_ROOT_PASSWORD
 
-# check the user has root priveleges or not
-if [ $USERID -ne 0 ]
-then
-    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
-    exit 1 #give other than 0 upto 127
-else
-    echo "You are running with root access" | tee -a $LOG_FILE
-fi
+app_setup
+maven_setup
+systemd_setup
 
-# validate functions takes input as exit status, what command they tried to install
-VALIDATE(){
-    if [ $1 -eq 0 ]
-    then
-        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
-    else
-        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
-        exit 1
-    fi
-}
+dnf install mysql -y  &>>$LOG_FILE
+VALIDATE $? "Install MySQL"
 
-dnf install python3 gcc python3-devel -y &>>$LOG_FILE
-VALIDATE $? "Install Python3 packages"
-
-id roboshop &>>$LOG_FILE
+mysql -h mysql.medaknaresh.digital -u root -p$MYSQL_ROOT_PASSWORD -e 'use cities' &>>$LOG_FILE
 if [ $? -ne 0 ]
 then
-    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
-    VALIDATE $? "Creating roboshop system user"
+    mysql -h mysql.medaknaresh.digital -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/schema.sql &>>$LOG_FILE
+    mysql -h mysql.medaknaresh.digital -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/app-user.sql  &>>$LOG_FILE
+    mysql -h mysql.medaknaresh.digital -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/master-data.sql &>>$LOG_FILE
+    VALIDATE $? "Loading data into MySQL"
 else
-    echo -e "System user roboshop already created ... $Y SKIPPING $N"
+    echo -e "Data is already loaded into MySQL ... $Y SKIPPING $N"
 fi
 
-mkdir -p /app 
-VALIDATE $? "Creating app directory"
+systemctl restart shipping &>>$LOG_FILE
+VALIDATE $? "Restart shipping"
 
-curl -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip &>>$LOG_FILE
-VALIDATE $? "Downloading payment"
-
-rm -rf /app/*
-cd /app 
-unzip /tmp/payment.zip &>>$LOG_FILE
-VALIDATE $? "unzipping payment"
-
-pip3 install -r requirements.txt &>>$LOG_FILE
-VALIDATE $? "Installing dependencies"
-
-cp $SCRIPT_DIR/payment.service /etc/systemd/system/payment.service &>>$LOG_FILE
-VALIDATE $? "Copying payment service"
-
-systemctl daemon-reload &>>$LOG_FILE
-VALIDATE $? "Daemon Reload"
-
-systemctl enable payment &>>$LOG_FILE
-VALIDATE $? "Enable payment"
-
-systemctl start payment &>>$LOG_FILE
-VALIDATE $? "Starting payment"
-
-END_TIME=$(date +%s)
-TOTAL_TIME=$(( $END_TIME - $START_TIME ))
-
-echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
+print_time
